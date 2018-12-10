223204c3da377db62808d3e732beb324cd6e17b3
AValerievna <a.valerievna12@gmail.com>
2018-12-10 17:08:53 +0300

    drafts

--- UnitTests/tests/file_class_test.py
+++ UnitTests/tests/file_class_test.py
@@ -1,2 +1,24 @@
+import pytest
+
+from proj.file import File
+
+
 class TestFile(object):
-    pass
+
+    @pytest.mark.parametrize("filename", [
+        ("pe", "r"),
+        ("ewd", "ew"),
+        ("tddf", "rla")
+    ])
+    def test_init(self, filename, content):
+        f = File.__init__(filename, content)
+        assert f._filename == filename and f._content == content, "Invalid init"
+
+    @pytest.mark.parametrize("filename", [
+        ("pe", "r"),
+        ("ewd", "ew"),
+        ("tddf", "rla")
+    ])
+    def test_init(self, filename, content):
+        f = File.__init__(filename, content)
+        assert f._filename == filename and f._content == content, "Invalid init"
--- UnitTests/tests/file_storage_test.py
+++ UnitTests/tests/file_storage_test.py
@@ -1,3 +1,35 @@
+import pytest
+
+from proj.file_storage import FileStorage
+
+
 class TestFileStorage(object):
-    def test_is_exist(self):
-        assert FileStorage.is_exists()
+    """def test_is_exist(self):
+      #  assert FileStorage.is_exists()"""
+
+    @pytest.mark.parametrize("size", [
+        3,
+        0,
+        100
+    ])
+    def test_init(self, size):
+        fs = FileStorage.__init__(size)
+        assert fs._max_size == size and fs._available_size == size and len(fs._files) == 0, "Invalid init"
+
+    @pytest.mark.parametrize("size", [
+        3,
+        0,
+        100
+    ])
+    def test_get_available_size(self, size):
+        fs = FileStorage.__init__(size)
+        assert fs.get_available_size() == size, "Invalid get_available_size"
+
+    @pytest.mark.parametrize("size", [
+        3,
+        0,
+        100
+    ])
+    def test_get_max_size(self, size):
+        fs = FileStorage.__init__(size)
+        assert fs.get_max_size() == size, "Invalid get_max_size"
